7bfefe23d66242f7940fff9b0e3cd4e948d06858
seockchan <sna12345@naver.com>
2022-04-11 18:36:58 +0900

    22.04.11_1

--- portfolio/react2022/README.md
+++ portfolio/react2022/README.md
@@ -1,4 +1,9 @@
 # 리액트
 
-## 리액트 설치
+# 리액트 설치
 npx create-react-app react2022
+
+# 리액트 실행
+cd react2022
+npm start y
+
--- portfolio/react2022/src/index.js
+++ portfolio/react2022/src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 컴퍼넌트 + props
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import react from "react";
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Hello(props){
+  return <h1>hello, {props.name}</h1>
+}
+const element = <Hello name="web"/>;
+
+ReactDOM.render(element, document.getElementById("root"));
