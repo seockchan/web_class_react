876c67c091af1f1c5cf5eff14ffba10796d7f613
seockchan <sna12345@naver.com>
2022-04-20 11:59:13 +0900

    22.04.20_2

--- react2022/src/components/pages/About.js
+++ react2022/src/components/pages/About.js
@@ -5,19 +5,93 @@ import Footer from "../layout/Footer";
 import Title from "../layout/Title";
 import Contact from "../layout/Contact";
 import AboutCont from "../includes/AboutCont";
+import Loading from "../basics/Loading";
+import { gsap } from "gsap";
 
-function About() {
-    return (
-        <>
-            <Header color="light" />
-            <Contents>
-                <Title title={["about","me"]}color="light"/>
-                <AboutCont color="light" />
-                <Contact />
-            </Contents>
-            <Footer color="light" />
-        </>
-    )
+class About extends React.Component {
+    state = {
+        isLoading : true,
+    }
+
+    mainAnimation=()=>{
+        setTimeout(() => {
+            gsap.to("#header",
+            {duration:0.4,
+            top:0,
+            ease: "slow(0.7, 0.7, false)"
+            })
+            gsap.to("#footer",
+            {duration:0.8,
+            bottom:0,
+            ease: "slow(0.7, 0.7, false)"
+            })
+            gsap.to(".cont__title strong",
+            {duration:0.5,
+                x:0,
+                y:0,
+                opacity:1,
+                delay:1,
+                ease: "slow(0.7, 0.7, false)"
+            })
+            gsap.to(".cont__title em",
+            {duration:0.5,
+                x:0,
+                y:0,
+                opacity:1,
+                delay:1.2,
+                ease: "slow(0.7, 0.7, false)"
+            })
+            gsap.to(".about__inner",
+            {duration:0.5,
+                x:0,
+                y:0,
+                opacity:1,
+                delay:1.5,
+                ease: "slow(0.7, 0.7, false)"
+            })
+        }, 10);
+    }
+
+    getPorts = async ()=>{
+        // console.log(ports);
+
+        setTimeout(() => {
+            console.log("두번째 시작");
+            this.setState({isLoading:false});
+            this.mainAnimation();
+        }, 1600);
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            console.log("첫번째 시작");
+            document.getElementById("loading").classList.remove("loading__active");
+            document.querySelector("body").style.background="#fff";
+            this.getPorts();
+        }, 2000);
+    }
+    render(){
+        const {isLoading} = this.state;
+
+        console.log();
+        return (
+            <>
+                {isLoading ? (
+                    <Loading color="light"/>
+                    ) : (
+                        <>
+                        <Header color="light" />
+                        <Contents>
+                            <Title title={["about","me"]}color="light"/>
+                            <AboutCont color="light" />
+                            <Contact />
+                        </Contents>
+                        <Footer color="light" />
+                    </>
+                )}
+            </>
+        )
+    }
 }
 
 export default About;
--- react2022/src/components/pages/Reference.js
+++ react2022/src/components/pages/Reference.js
@@ -5,19 +5,96 @@ import Footer from "../layout/Footer";
 import Title from "../layout/Title";
 import Contact from "../layout/Contact";
 import ReferCont from "../includes/ReferCont";
+import Loading from "../basics/Loading";
+import { gsap } from "gsap";
 
-function Reference() {
-    return (
-        <>
-            <Header color="light" />
-            <Contents>
-                <Title title={["Reference","Book"]}color="light"/>
-                <ReferCont color="light" />
-                <Contact />
-            </Contents>
-            <Footer color="light" />
-        </>
-    )
+
+class Reference extends React.Component {
+    state = {
+        isLoading : true,
+    }
+
+    mainAnimation=()=>{
+        setTimeout(() => {
+            gsap.to("#header",
+            {duration:0.4,
+            top:0,
+            ease: "slow(0.7, 0.7, false)"
+            })
+            gsap.to("#footer",
+            {duration:0.8,
+            bottom:0,
+            ease: "slow(0.7, 0.7, false)"
+            })
+            gsap.to(".cont__title strong",
+            {duration:0.5,
+                x:0,
+                y:0,
+                opacity:1,
+                delay:1,
+                ease: "slow(0.7, 0.7, false)"
+            })
+            gsap.to(".cont__title em",
+            {duration:0.5,
+                x:0,
+                y:0,
+                opacity:1,
+                delay:1.2,
+                ease: "slow(0.7, 0.7, false)"
+            })
+            gsap.to(".refer__inner",
+            {duration:0.5,
+                x:0,
+                y:0,
+                opacity:1,
+                delay:1.5,
+                ease: "slow(0.7, 0.7, false)"
+            })
+        }, 10);
+    }
+
+    getPorts = async ()=>{
+        
+
+        // console.log(ports);
+
+        setTimeout(() => {
+            console.log("두번째 시작");
+            this.setState({isLoading:false});
+            this.mainAnimation();
+        }, 1600);
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            console.log("첫번째 시작");
+            document.getElementById("loading").classList.remove("loading__active");
+            document.querySelector("body").style.background="#fff";
+            this.getPorts();
+        }, 2000);
+    }
+    render(){
+        const {isLoading} = this.state;
+
+        console.log();
+        return (
+            <>
+                {isLoading ? (
+                    <Loading color="light"/>
+                    ) : (
+                        <>
+                        <Header color="light" />
+                        <Contents>
+                            <Title title={["Reference","Book"]}color="light"/>
+                            <ReferCont color="light" />
+                            <Contact />
+                        </Contents>
+                        <Footer color="light" />
+                    </>
+                )}
+            </>
+        )
+    }
 }
 
 export default Reference;
